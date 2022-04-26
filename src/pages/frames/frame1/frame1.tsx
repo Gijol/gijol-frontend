880f09dfc85cddf2ca3a7dff8a1bc1d2639aad17
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-04-27 01:27:18 +0900

    feat: main page소개 및 버튼 제작하기

--- src/pages/frames/frame1/frame1.tsx
+++ src/pages/frames/frame1/frame1.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FrameContainer } from './styles';
+import ButtonContainer from './buttonBox/buttons';
+
+const Title = styled.h1`
+  white-space: pre;
+  font-weight: 700;
+  margin-top: 170px;
+  z-index: 10;
+  font-style: normal;
+  color: ${(props) => props.theme.color.main};
+  font-size: 60px;
+  letter-spacing: 0px;
+  line-height: 1.7;
+  text-align: center;
+`;
+
+const Blur = styled.div`
+  position: absolute;
+  /* z-index: 0; */
+  /* top: 0px; */
+  left: 0;
+  width: 100%;
+  height: 500px;
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(
+    180deg,
+    white 0%,
+    white 59px,
+    rgba(255, 255, 255, 0) 100%
+  );
+`;
 
 const Frame1 = function Frame1(): JSX.Element {
-  return <FrameContainer>Hello</FrameContainer>;
+  return (
+    <FrameContainer>
+      <Blur />
+      <Title>
+        졸업요건 <br />
+        Gijol에서 간단하게 확인하세요.
+      </Title>
+      <ButtonContainer />
+    </FrameContainer>
+  );
 };
 
 export default Frame1;
--- src/pages/frames/frame1/styles.ts
+++ src/pages/frames/frame1/styles.ts
@@ -4,9 +4,10 @@ const FrameContainer = styled.div`
   max-width: ${(props) => props.theme.basicWidth};
   height: 100vh;
   display: flex;
+  flex-direction: column;
   background-color: aqua;
   overflow: hidden;
-
+  /* justify-content: center; */
   margin-left: auto;
   margin-right: auto;
 `;
