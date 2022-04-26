06716ed5f60a42c0c25ae630ad5d64f750d21dee
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-04-27 01:29:59 +0900

    refactor: style code 각 폴더의 styles.ts로 이동

--- src/pages/frames/frame1/frame1.tsx
+++ src/pages/frames/frame1/frame1.tsx
@@ -1,37 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FrameContainer } from './styles';
+import { FrameContainer, Blur, Title } from './styles';
 import ButtonContainer from './buttonBox/buttons';
-
-const Title = styled.h1`
-  white-space: pre;
-  font-weight: 700;
-  margin-top: 170px;
-  z-index: 10;
-  font-style: normal;
-  color: ${(props) => props.theme.color.main};
-  font-size: 60px;
-  letter-spacing: 0px;
-  line-height: 1.7;
-  text-align: center;
-`;
-
-const Blur = styled.div`
-  position: absolute;
-  /* z-index: 0; */
-  /* top: 0px; */
-  left: 0;
-  width: 100%;
-  height: 500px;
-  margin: 0;
-  padding: 0;
-  background: linear-gradient(
-    180deg,
-    white 0%,
-    white 59px,
-    rgba(255, 255, 255, 0) 100%
-  );
-`;
 
 const Frame1 = function Frame1(): JSX.Element {
   return (
--- src/pages/frames/frame1/styles.ts
+++ src/pages/frames/frame1/styles.ts
@@ -11,5 +11,34 @@ const FrameContainer = styled.div`
   margin-left: auto;
   margin-right: auto;
 `;
+const Title = styled.h1`
+  white-space: pre;
+  font-weight: 700;
+  margin-top: 170px;
+  z-index: 10;
+  font-style: normal;
+  color: ${(props) => props.theme.color.main};
+  font-size: 60px;
+  letter-spacing: 0px;
+  line-height: 1.7;
+  text-align: center;
+`;
 
-export { FrameContainer };
+const Blur = styled.div`
+  position: absolute;
+  /* z-index: 0; */
+  /* top: 0px; */
+  left: 0;
+  width: 100%;
+  height: 500px;
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(
+    180deg,
+    white 0%,
+    white 59px,
+    rgba(255, 255, 255, 0) 100%
+  );
+`;
+
+export { FrameContainer, Title, Blur };
