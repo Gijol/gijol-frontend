ffee4028e6048121386e7bfa739c59ee6d45ccfc
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-04-29 17:13:44 +0900

    feat: 파일 add compoenent 제작

---
+++ src/pages/frames/frameFileAdd/fildAdd.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FileAdd = styled.section`
+  max-width: ${(props) => props.theme.basicWidth};
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: beige;
+`;
+
+const FileAddFrame = function FileAddFrame(): JSX.Element => {
+   
+}
