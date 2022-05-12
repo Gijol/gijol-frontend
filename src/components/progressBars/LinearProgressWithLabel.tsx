81213fac590945e14ac8535172faa4379cec79db
Dongho Seo <ehcws333@gmail.com>
2022-05-12 22:41:00 +0900

    feat: 애니메이션 로딩 중 js소수점 오류 수정

--- src/components/progressBars/LinearProgressWithLabel.tsx
+++ src/components/progressBars/LinearProgressWithLabel.tsx
@@ -29,7 +29,7 @@ const LinearProgressWithLabel =
               fill: ({ datum }) => datum.fill,
             },
           }}
-          labels={({ datum }) => `${datum.y}%`}
+          labels={({ datum }) => `${Math.round(datum.y)}%`}
         />
         <VictoryAxis
           crossAxis={false}
