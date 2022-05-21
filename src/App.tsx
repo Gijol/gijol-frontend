0d43c8685a5f068afe45f6d868bab686eca737c8
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-05-21 16:12:17 +0900

    feat: react ga 생성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
+import ReactGA from 'react-ga';
 
 import Main from 'pages/main';
 import Qna from 'pages/qna';
@@ -10,7 +11,23 @@ import ResultPage from 'pages/result';
 import Result from 'utils/api/result';
 import MobileMain from 'pages/mobile/mobileMain';
 
+ReactGA.event({
+  category: 'User',
+  action: 'Create an Account',
+});
+ReactGA.exception({
+  description: 'An error ocurred',
+  fatal: true,
+});
+
 function App() {
+  // const location = useLocation();
+  const REENV = process.env.REACT_APP_DEV_GOOGLE_ANALYTICS_TRACKING_ID;
+  useEffect(() => {
+    ReactGA.initialize(REENV!);
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }, [window.location]);
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
