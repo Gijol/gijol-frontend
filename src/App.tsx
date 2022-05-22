a14906501d8192cdfb2ae7551d02f07266f6799b
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-05-22 22:43:56 +0900

    feat: notion page connect

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { theme } from 'styles/theme';
 import ReactGA from 'react-ga';
 
 import Main from 'pages/main';
-import Qna from 'pages/qna';
-import AboutUs from 'pages/aboutus';
 import ResultPage from 'pages/result';
 import Result from 'utils/api/result';
 import MobileMain from 'pages/mobile/mobileMain';
@@ -34,8 +32,6 @@ function App() {
         <Routes>
           <Route path="/">
             <Route path="" element={<Main />} />
-            <Route path="qna" element={<Qna />} />
-            <Route path="aboutus" element={<AboutUs />} />
             <Route path="result" element={<ResultPage />} />
             <Route path="mobile" element={<MobileMain />} />
           </Route>
--- src/common/header/navigation.tsx
+++ src/common/header/navigation.tsx
@@ -3,11 +3,23 @@ import { RightNav } from './styles';
 const Navi = function Navi(): JSX.Element {
   return (
     <>
-      <RightNav to="/qna">
-        <span>Q&A</span>
+      <RightNav>
+        <a
+          href="https://choieungi-project.notion.site/Q-A-9e325eabef4e479a8f47e95eb90bb344"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Q&A
+        </a>
       </RightNav>
-      <RightNav to="/aboutus">
-        <span>About Us</span>
+      <RightNav>
+        <a
+          href="https://choieungi-project.notion.site/Team-Gijol-e5e279e91a0646c2a84b548873601a3f"
+          rel="noreferrer"
+          target="_blank"
+        >
+          About Us
+        </a>
       </RightNav>
     </>
   );
